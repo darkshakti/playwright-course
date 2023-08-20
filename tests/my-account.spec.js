9aaa46a7171c9aa7a7913b74e8e20c9a7716fc70
Margarita Vladyko <margo.vladyko@gmail.com>
2023-08-21 01:13:20 +0300

    add getLoginToken

--- tests/my-account.spec.js
+++ tests/my-account.spec.js
@@ -3,6 +3,8 @@ import { MyAccountPage } from "../page-objects/MyAccountPage";
 
 test.only("My Account using cookie injection", async ({ page }) => {
     // Make a request to get login token
+    const loginToken = await getLoginToken();
+    console.warn({loginToken});
     // Inject the login token into the browser
     const myAccount = new MyAccountPage(page);
     await myAccount.visit();
